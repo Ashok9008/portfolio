023aa3fbb18f73efd8bda3159d65d53338e741df
Ashok <mahajanashok0709@gmail.com>
2023-09-04 12:19:59 +0530

    url3

--- src/Page/Contact.js
+++ src/Page/Contact.js
@@ -24,7 +24,7 @@ const Contact = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setButtonText("Sending...");
-    let response = await fetch("https://portfolio-backend-git-main-ashok9008.vercel.app/", {
+    let response = await fetch("https://portfolio-backend-api-git-main-ashok9008.vercel.app/contact", {
       method: "POST",
       headers: {
         "Content-Type": "application/json;charset=utf-8",
