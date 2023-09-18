fd15f6b03757bc949272ec904cf52d82600709ff
Ashok <mahajanashok0709@gmail.com>
2023-09-18 18:05:02 +0530

    todoList

--- src/Page/Projects.js
+++ src/Page/Projects.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './projacts.css';
 import Img5 from '../images/img-5.png'
 import Img6 from '../images/img-6.png'
+import todo from '../images/todo.png'
 import BottomLine from '../components/BottomLine';
 // import { Link } from 'react-router-dom';
 
@@ -16,14 +17,14 @@ const Projects = () => {
       <div className='main'>
         <div className="card">
           <div className="image">
-            <img src={Img5} alt="" />
+            <img src={todo} alt="" />
           </div>
           <div className="text">
-            <h2>Portfolio</h2>
+            <h2>Todo list</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo fugiat velit 4 odio laudantium ad doloremque maiores  aut quas excepturi voluptates?</p>
             <div className="button">
-             <a href="#fakeLink" class="btn1 black circular">GITHUB</a>
-            <a href="#fakeLink" class="btn1 black circular">PERVIEW</a>
+             <a href="https://github.com/Ashok9008/Todo-list" target='blank' className="btn1 black circular">GITHUB</a>
+            <a href="https://todo-list-git-main-ashok9008.vercel.app/" target='blank' className="btn1 black circular">PERVIEW</a>
           </div>
           </div>
           
@@ -36,8 +37,8 @@ const Projects = () => {
             <h2> Food lab</h2>
             <p>This Is Foodlab Webpage, it was Make In Only HTML5 And CSS3, Foodlab Is Fully Responsive Webpage, Foodlab Is My First Project in Web Development.</p>
             <div className="button">
-            <a href="https://github.com/Ashok9008/Maxicon_Food" target='blank' class="btn1 black circular">GITHUB</a>
-            <a href="https://maxicon-food.vercel.app/" target='blank' class="btn1 black circular">PERVIEW</a>
+            <a href="https://github.com/Ashok9008/Maxicon_Food" target='blank' className="btn1 black circular">GITHUB</a>
+            <a href="https://maxicon-food.vercel.app/" target='blank' className="btn1 black circular">PERVIEW</a>
           </div>
           </div>
           
@@ -47,12 +48,12 @@ const Projects = () => {
             <img src={Img5} alt="" />
           </div>
           <div className="text">
-            <h2>Project3</h2>
+            <h2>Portfolio</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo fugiat velit 4 odio laudantium ad doloremque maiores  aut quas excepturi voluptates?</p>
             <div className="button">
             
-            <a href="#fakeLink" class="btn1 black circular">GITHUB</a>
-            <a href="#fakeLink" class="btn1 black circular">PERVIEW</a>
+            <a href="https://github.com/Ashok9008/portfolio" target='blnak ' className="btn1 black circular">GITHUB</a>
+            <a href="https://portfolio-git-main-ashok9008.vercel.app/" target='blank' className="btn1 black circular">PERVIEW</a>
               
           </div>
           </div>
